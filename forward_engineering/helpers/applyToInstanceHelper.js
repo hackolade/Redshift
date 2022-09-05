113fb3e4bf2201b68773e80b6821c6324e062388
DrMyroslav <myroslav.druchkiv@binary-studio.com>
2022-09-05 13:25:08 +0300

    added serverless: delete set global dependecy from apply to instance

--- forward_engineering/helpers/applyToInstanceHelper.js
+++ forward_engineering/helpers/applyToInstanceHelper.js
@@ -1,11 +1,22 @@
 const redshiftHelper = require('../../reverse_engineering/helpers/redshiftHelper');
-const { setDependencies, dependencies } = require('../../reverse_engineering/helpers/appDependencies');
+const { redshift, redshiftData, redshiftLess } = require('../../reverse_engineering/helpers/appDependencies');
+
+const setDependencies = app => {
+	redshiftHelper.setDependencies({
+		lodash: app.require('lodash'), 
+		aws: {
+			redshift,
+			redshiftData,
+			redshiftLess,
+		}
+	});
+};
 
 module.exports = {
 	async applyToInstance(connectionInfo, logger, app) {
 		const script = connectionInfo.script;
 		setDependencies(app);
-		redshiftHelper.setDependencies(dependencies);
+
 		await redshiftHelper.connect(connectionInfo, logger);
 
 		logger.log(
@@ -21,7 +32,6 @@ module.exports = {
 
 	async testConnection(connectionInfo, logger, app) {
 		setDependencies(app);
-		redshiftHelper.setDependencies(dependencies);
 
 		await redshiftHelper.testConnection(connectionInfo, logger);
 	},
--- reverse_engineering/helpers/appDependencies.js
+++ reverse_engineering/helpers/appDependencies.js
@@ -1,15 +1,22 @@
+const redshift = require("@aws-sdk/client-redshift");
+const redshiftData = require("@aws-sdk/client-redshift-data");
+const redshiftLess = require("@aws-sdk/client-redshift-serverless");
+
 let dependencies = {};
 
 const setDependencies = app => {
 	dependencies.lodash = app.require('lodash');
 	dependencies.aws = {
-		redshift: require("@aws-sdk/client-redshift"),
-		redshiftData: require("@aws-sdk/client-redshift-data"),
-		redshiftLess: require("@aws-sdk/client-redshift-serverless")
+		redshift,
+		redshiftData,
+		redshiftLess
 	};
 };
 
 module.exports = {
 	setDependencies,
 	dependencies,
+	redshift,
+	redshiftData,
+	redshiftLess,
 };
