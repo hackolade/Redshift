a30d1759022b7a9cf49378c044876944caa06e18
Serhii Filonenko <91055067+serhii-filonenko@users.noreply.github.com>
2025-02-27 12:06:03 +0200

    HCK-10154: enable dbt feature (#54)
    * HCK-10154: add dbt provider
    
    * HCK-10154: enable dbt feature

--- forward_engineering/helpers/constraintHelper.js
+++ forward_engineering/helpers/constraintHelper.js
@@ -1,3 +1,12 @@
+/**
+ * @typedef {import('../types').ColumnDefinition} ColumnDefinition
+ * @typedef {import('../types').ConstraintDtoColumn} ConstraintDtoColumn
+ * @typedef {import('../types').ConstraintDto} ConstraintDto
+ * @typedef {import('../types').JsonSchema} JsonSchema
+ */
+
+const _ = require('lodash');
+
 module.exports = app => {
 	const { foreignKeysToString, foreignActiveKeysToString } = require('./general')(app);
 	const assignTemplates = app.require('@hackolade/ddl-fe-utils').assignTemplates;
@@ -20,7 +29,141 @@ module.exports = app => {
 		};
 	};
 
+	/**
+	 * @param {ColumnDefinition} columnDefinition
+	 * @returns {boolean}
+	 */
+	const isPrimaryKey = columnDefinition => {
+		return !columnDefinition.compositePrimaryKey && columnDefinition.primaryKey;
+	};
+
+	/**
+	 * @param {ColumnDefinition} columnDefinition
+	 * @returns {boolean}
+	 */
+	const isUniqueKey = columnDefinition => {
+		return !columnDefinition.compositeUniqueKey && columnDefinition.unique;
+	};
+
+	/**
+	 * @param {string} keyId
+	 * @param {Record<string, JsonSchema>} properties
+	 * @returns {string}
+	 */
+	const findName = (keyId, properties) => {
+		return Object.keys(properties).find(name => properties[name].GUID === keyId);
+	};
+
+	/**
+	 * @param {string} keyId
+	 * @param {Record<string, JsonSchema>} properties
+	 * @returns {boolean}
+	 */
+	const checkIfActivated = (keyId, properties) => {
+		return _.get(
+			Object.values(properties).find(prop => prop.GUID === keyId),
+			'isActivated',
+			true,
+		);
+	};
+
+	/**
+	 * @param {Array<{ keyId: string }>} keys
+	 * @param {JsonSchema} jsonSchema
+	 * @returns {ConstraintDtoColumn}
+	 */
+	const getKeys = (keys, jsonSchema) => {
+		return _.map(keys, key => {
+			return {
+				name: findName(key.keyId, jsonSchema.properties),
+				isActivated: checkIfActivated(key.keyId, jsonSchema.properties),
+			};
+		});
+	};
+
+	/**
+	 * @param {{ jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	const getCompositePrimaryKeys = ({ jsonSchema }) => {
+		if (!Array.isArray(jsonSchema.primaryKey)) {
+			return [];
+		}
+
+		return jsonSchema.primaryKey
+			.filter(primaryKey => !_.isEmpty(primaryKey.compositePrimaryKey))
+			.map(primaryKey => ({
+				keyType: 'PRIMARY KEY',
+				columns: getKeys(primaryKey.compositePrimaryKey, jsonSchema),
+			}));
+	};
+
+	/**
+	 * @param {{ jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	const getCompositeUniqueKeys = ({ jsonSchema }) => {
+		if (!Array.isArray(jsonSchema.uniqueKey)) {
+			return [];
+		}
+
+		return jsonSchema.uniqueKey
+			.filter(uniqueKey => !_.isEmpty(uniqueKey.compositeUniqueKey))
+			.map(uniqueKey => ({
+				keyType: 'UNIQUE',
+				columns: getKeys(uniqueKey.compositeUniqueKey, jsonSchema),
+			}));
+	};
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition }}
+	 * @returns {ConstraintDto | undefined}
+	 */
+	const getPrimaryKeyConstraint = ({ columnDefinition }) => {
+		if (!isPrimaryKey(columnDefinition)) {
+			return;
+		}
+
+		return {
+			keyType: 'PRIMARY KEY',
+		};
+	};
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition }}
+	 * @returns {ConstraintDto | undefined}
+	 */
+	const getUniqueKeyConstraint = ({ columnDefinition }) => {
+		if (!isUniqueKey(columnDefinition)) {
+			return;
+		}
+
+		return {
+			keyType: 'UNIQUE',
+		};
+	};
+
+	const getCompositeKeyConstraints = ({ jsonSchema }) => {
+		const compositePrimaryKeys = getCompositePrimaryKeys({ jsonSchema });
+		const compositeUniqueKeys = getCompositeUniqueKeys({ jsonSchema });
+
+		return [...compositePrimaryKeys, ...compositeUniqueKeys];
+	};
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition }}
+	 * @returns {ConstraintDto[]}
+	 */
+	const getColumnConstraints = ({ columnDefinition }) => {
+		const primaryKeyConstraint = getPrimaryKeyConstraint({ columnDefinition });
+		const uniqueKeyConstraint = getUniqueKeyConstraint({ columnDefinition });
+
+		return [primaryKeyConstraint, uniqueKeyConstraint].filter(Boolean);
+	};
+
 	return {
 		generateConstraint,
+		getCompositeKeyConstraints,
+		getColumnConstraints,
 	};
 };
